434c06b00290c1c291df33a88bc19944617eed1c
sammid37 <samanthadmedeiros37@gmail.com>
2022-09-13 19:41:20 -0300

    👋 Hello World

--- hello_world.ex
+++ hello_world.ex
@@ -1,6 +1,7 @@
 # Linguagem de Programação III c/ Elixir
 # Samantha D. Medeiros
 # Hello World
+# https://exercism.org/tracks/elixir/exercises/hello-world/solutions/sammid37
 
 defmodule HelloWorld do
   def hello do
